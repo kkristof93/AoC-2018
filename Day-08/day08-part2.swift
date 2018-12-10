b0bd426deb6ac4cd7c5e6cb2d35bb7aa1488ddef
Kristóf Koncz <konczkristof93@gmail.com>
2018-12-10 01:12:01 +0100

    Day 8

---
+++ Day-08/day08-part2.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+struct Node {
+	let number: Int
+	var childCount: Int = 0
+	var metadataCount: Int = 0
+	var parent: Int?
+	var children = [Int]()
+	var metadata = [Int]()
+
+	init(number: Int, parent: Int?) {
+		self.number = number
+		self.parent = parent
+	}
+}
+
+enum State {
+	case childCount(Int?)
+	case metadataCount
+	case metadata(Int, Int)
+}
+
+let file = try! String(contentsOfFile: "input.txt", encoding: .utf8)
+let numbers = file.components(separatedBy: " ")
+
+var tree = [Node]()
+
+var current = 0
+var state: State = .childCount(nil)
+
+for num in numbers {
+	let number = Int(num)!
+
+	switch state {
+	case .childCount(let parent):
+		tree.insert(Node(number: current, parent: parent), at: current)
+		tree[current].childCount = number
+
+		if let parent = parent {
+			tree[parent].children.append(current)
+		}
+
+		state = .metadataCount
+
+	case .metadataCount:
+		tree[current].metadataCount = number
+		if tree[current].childCount == 0 {
+			state = .metadata(current, number)
+		} else {
+			state = .childCount(current)
+			current += 1
+		}
+
+	case .metadata(let index, let count):
+		tree[index].metadata.append(number)
+		if count - 1 > 0 {
+			state = .metadata(index, count - 1)
+		} else {
+			if let parent = tree[index].parent {
+				if tree[parent].childCount != tree[parent].children.count {
+					current += 1
+					state = .childCount(parent)
+				} else {
+					state = .metadata(parent, tree[parent].metadataCount)
+				}
+			}
+			
+		}
+	}
+}
+
+func getValue(_ nodeNumber: Int) -> Int {
+	let node = tree[nodeNumber]
+
+	if node.childCount == 0 {
+		return node.metadata.reduce(0, +)
+	}
+
+	let values = node.metadata.compactMap { 
+		($0 <= node.children.count && $0 > 0) ? getValue(node.children[$0 - 1]) : nil 
+	}
+
+	return values.reduce(0, +)
+}
+
+let rootValue = getValue(0)
+print(rootValue)
+
